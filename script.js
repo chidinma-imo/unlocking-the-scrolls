c5a9c8164fe1708acbcfcfc5439ec4fae901e9e0
chidinma-imo <contact.chidinmaimo@gmail.com>
2026-09-23 10:27:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-const openButton = document.getElementById("openScroll");
-
-openButton.addEventListener("click", function () {
-
+document.getElementById("openScroll").onclick = function() {
     document.getElementById("archive").scrollIntoView({
         behavior: "smooth"
     });
-
-});
+};
