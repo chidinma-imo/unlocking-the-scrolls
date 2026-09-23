303ac2c4095f9819ee3fc388fe123cfaae3c363d
chidinma-imo <contact.chidinmaimo@gmail.com>
2026-09-23 09:59:55 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const enterButton = document.getElementById("enterButton");
+const archive = document.getElementById("archive");
+
+enterButton.addEventListener("click", () => {
+
+    archive.scrollIntoView({
+        behavior: "smooth"
+    });
+
+});
